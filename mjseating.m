b4616e5d70deb8b1a180eaa65c67591064b25d1f
max ulidtko <ulidtko@gmail.com>
2015-10-21 18:47:52 +0300

    cleanup before rewrite

--- mjseating.m
+++ mjseating.m
@@ -6,15 +6,14 @@
 :- import_module set.
 
 :- type player == int.
-%:- type player ---> p1; p2; p3; p4; p5; p6; p7; p8; p9; p10; p11; p12; p13; p14; p15; p16.
-%Players = set([p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16]).
 
 :- type table == set(player). % exactly 4 players
 :- type hanchan == list(table).
 
 :- type metDB == set({player, player}).
-:- func emptyDB = metDB.
-:- func mergeDBs(metDB, metDB) = metDB.
+
+:- type quad(T) == {T, T, T, T}.
+:- type pquads == set(quad(player)).
 
 :- pred main(io::di, io::uo) is cc_multi.
 
@@ -24,31 +23,41 @@
 :- import_module char, string.
 :- import_module solutions.
 
-:- func fst({T1, T2}) = T1.
-fst({X, _}) = X.
-:- func snd({T1, T2}) = T2.
-snd({_, X}) = X.
+%----- UTILITIES -----
 
-emptyDB = set([]).
-mergeDBs(A, B) = union(A, B).
-
-:- pred alreadyMet(metDB::in, player::in, player::in) is semidet.
-alreadyMet(D, P1, P2) :- member({P1, P2}, D); member({P2, P1}, D).
-
-:- pred neverMet(metDB::in, {player, player}::in) is semidet.
+:- pred neverMet(metDB::in, {player,player}::in) is semidet.
 neverMet(D, PP) :- not member(PP, D).
 
-:- func tablePairs(table) = metDB.
-%tablePairs(T) = solutions_set(
-%        (pred({X1, X2}::out) is nondet :-
-%            member(X1, T), member(X2, T), compare((<), X1, X2))
-%    ).
+:- func fst({T1, T2, T3, T4}) = T1.
+fst({X, _, _, _}) = X.
+:- func snd({T1, T2, T3, T4}) = T2.
+snd({_, X, _, _}) = X.
+:- func thd({T1, T2, T3, T4}) = T3.
+thd({_, _, X, _}) = X.
+:- func fth({T1, T2, T3, T4}) = T4.
+fth({_, _, _, X}) = X.
+
+
+:- func tablePairs(table) = set({player,player}).
 tablePairs(T) = set([{A,B}, {A,C}, {A,D}, {B,C}, {B,D}, {C,D}]) :-
     L = to_sorted_list(T),
     A = det_index1(L, 1),
     B = det_index1(L, 2),
     C = det_index1(L, 3),
     D = det_index1(L, 4).
+
+
+%----- CORE LOGIC -----
+
+:- pred allQuads(set(player)::in, pquads::out) is det.
+allQuads(Players, Quads) :-
+    solutions_set((
+        pred({PA,PB,PC,PD}::out) is nondet :-
+            member(PA, Players),
+            member(PB, Players), PB > PA,
+            member(PC, Players), PC > PB,
+            member(PD, Players), PD > PC
+    ), Quads).
 
 
 :- pred fillTable(
@@ -60,52 +69,12 @@ tablePairs(T) = set([{A,B}, {A,C}, {A,D}, {B,C}, {B,D}, {C,D}]) :-
     ) is nondet.
 
 fillTable(Table, Ps, PsRest, DB, DBout) :-
-    (compare((>), count(Ps), 4); checkConstraints(Ps, _, DB, _)),
+    %(compare((>), count(Ps), 4); checkConstraints(Ps, _, DB, _)),
 
     subsetExact4(Ps, Table, Pairs, neverMet(DB)),
     %is_empty(intersect(Pairs, DB)),
-    PsRest = difference(Ps, Table),
-    DBout = mergeDBs(DB, Pairs).
-
-
-:- pred checkConstraints(
-      set(player)::in
-    , table::out
-    , metDB::in
-    , metDB::out
-    ) is nondet.
-checkConstraints(FreePlayers, set([]), DB, DB) :-
-    FreePlayersL = to_sorted_list(FreePlayers),
-    N = count(FreePlayers),
-
-    %-- filter out EligiblePairs
-    solutions(
-        (pred({P1,P2}::out) is nondet :-
-            member(K1,    1..N), P1 = det_index1(FreePlayersL, K1),
-            member(K2, K1+1..N), P2 = det_index1(FreePlayersL, K2),
-            neverMet(DB, {P1, P2})
-        ),
-        EligiblePairs
-    ),
-
-    EligiblePair = (pred(PA::in, PB::out) is nondet :-
-        member({PA,PB}, EligiblePairs); member({PB,PA}, EligiblePairs)
-    ),
-
-    %-- every FreePlayer has at least 3 eligible partners
-    all [P] (member(P, FreePlayersL),
-        Partners = solutions(pred(Q::out) is nondet :- EligiblePair(P, Q)),
-        length(Partners) >= 3
-    )
-    .
-
-:- pred eligiblePartner(
-      metDB::in
-    , player::out
-    , player::out
-    ) is nondet.
-eligiblePartner(DB, PA, PB) :- member({PA,PB}, DB).
-eligiblePartner(DB, PB, PA) :- member({PA,PB}, DB).
+    PsRest = set.difference(Ps, Table),
+    DBout = set.union(DB, Pairs).
 
 
 :- pred subsetExact4(
@@ -151,7 +120,7 @@ fillNTables(0, [], _, D, D).
 fillNTables(N, Tables, Players, DB, DBout) :-
     fillTable(Table0, Players, PlayersRest, DB, DBupd1),
     fillNTables(N - 1, TablesN1, PlayersRest, DB, DBupd2),
-    DBout = mergeDBs(DBupd1, DBupd2),
+    DBout = set.union(DBupd1, DBupd2),
     Tables = [Table0 | TablesN1].
 
 
@@ -167,7 +136,7 @@ fillAllTables([], set([]), D, D).
 fillAllTables(Tables, Players, DB, DBout) :-
     fillTable(Table0, Players, PlayersRest, DB, DBupd1),
     fillAllTables(TablesN1, PlayersRest, DB, DBupd2),
-    DBout = mergeDBs(DBupd1, DBupd2),
+    DBout = set.union(DBupd1, DBupd2),
     Tables = [Table0 | TablesN1].
 
 
@@ -177,89 +146,20 @@ fillAllTables(Tables, Players, DB, DBout) :-
     , list(hanchan)::out
     , metDB::out
     ) is nondet.
-searchNHanchans(0,       _, [], emptyDB).
+searchNHanchans(0,       _, [], set.init).
 searchNHanchans(N, Players, [H0 | Hn1], DBupd) :-
     N > 0,
     fillAllTables(H0, Players, DBbase, DBupd),
     trace [io(!IO)] (
         io.write_string("== Candidate hanchan " ++ string(N) ++ " ==", !IO), io.nl(!IO),
         io.write_string("  Tables: " ++ string(H0), !IO), io.nl(!IO),
-        io.write_string("  DB: " ++ pprintDB2Dot(DBupd), !IO), io.nl(!IO),
-        io.write_string("  C(DB): " ++ pprintDB2Dot(dbComplement(DBupd)), !IO), io.nl(!IO)
+        io.write_string("  DB: " ++ pprintDB2Dot(DBupd), !IO), io.nl(!IO)
+        %io.write_string("  C(DB): " ++ pprintDB2Dot(dbComplement(DBupd)), !IO), io.nl(!IO)
     ),
     searchNHanchans(N - 1, Players, Hn1, DBbase).
 
 
-:- pred fourHanchan4Tables(
-      set(player)::in
-    , list(table)::out
-    , list(table)::out
-    , list(table)::out
-    , list(table)::out
-    ) is nondet.
-fourHanchan4Tables(Players, H1, H2, H3, H4) :-
-    fillNTables(4, H1, Players, emptyDB, DB2),
-    fillNTables(4, H2, Players, DB2, DB3),
-    fillNTables(4, H3, Players, DB3, DB4),
-    fillNTables(4, H4, Players, DB4, _).
-
-
-
-:- pred subsetExactN(int::in, set(T)::in, set(T)::out) is nondet.
-:- pred subsetExactNCond(
-      int::in
-    , pred(T)::in(pred(in) is semidet)
-    , set(T)::in
-    , set(T)::out
-    ) is nondet.
-
-subsetExactN(0,   _, set([])).
-subsetExactN(N, SIn, Ans) :-
-    %member(X, SIn),
-    %remove(X, SIn, S),
-    %P = (pred(Y::in) is semidet :- compare((<), X, Y)),
-    %divide(P, S, SRest, _),
-    setSplit(SIn, _, X, SRest),
-
-    subsetExactN(N - 1, SRest, AnsRest),
-    Ans = insert(AnsRest, X).
-
-:- pred setSplit(set(T)::in, set(T)::out, T::out, set(T)::out) is nondet.
-% intentionally fails on empty input set
-setSplit(S, init, X, init) :- singleton_set(X, S).
-setSplit(S, set(Prefix), X, set(Suffix)) :-
-    N = count(S),
-    N > 1,
-    L = to_sorted_list(S),
-    list.member(K, 1..N),
-    split_list(K-1, L, Prefix, [X | Suffix])
-    .
-
-
-:- func dbComplement(metDB) = metDB.
-dbComplement(D) = R :-
-    Nodes = to_sorted_list(union(map(fst, D), map(snd, D))),
-    R = solutions_set((
-        pred({N1, N2}::out) is nondet :-
-            member(N1, Nodes),
-            member(N2, Nodes),
-            N2 > N1,
-            neverMet(D, {N1, N2})
-    )).
-
-subsetExactNCond(0,  _,   _, set([])).
-subsetExactNCond(N, UP, SIn, Ans) :-
-    member(X, SIn),
-    remove(X, SIn, S),
-
-    P = (pred(Y::in) is semidet :- compare((<), X, Y), UP(Y)),
-    divide(P, S, SRest, _),
-
-    subsetExactNCond(N - 1, UP, SRest, AnsRest),
-    Ans = insert(AnsRest, X).
-
-
-
+%----- PRETTY PRINTING -----
 
 :- func pprintHanchan(list(table)) = list(list(player)).
 pprintHanchan(H) = map(to_sorted_list, H).
@@ -269,6 +169,7 @@ pprintDB2Dot(DB) = "strict graph { " ++ join_list("; ", Lines) ++ "}" :-
     Lines = map(func({P1,P2}) = string(P1) ++ " -- " ++ string(P2), to_sorted_list(DB))
     .
 
+%----- CMDLINE -----
 
 :- import_module getopt_io.
 :- type myoption ---> help; playerCount; hanchanCount.
@@ -314,6 +215,8 @@ main -->
         print(usage),
         set_exit_status(1)
     ).
+
+%----- MAIN -----
 
 :- pred main(option_table(myoption)::in, io::di, io::uo) is cc_multi.
 main(Options, !IO) :-
